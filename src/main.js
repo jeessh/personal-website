a322e61f8f2c53302b2000dea1fa87ce6c6cc0d3
jeessh <jesshuang5733@gmail.com>
2022-01-23 17:36:51 -0500

    commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ResumePage from './pages/resume'
-import Home from './pages';
+import Home from './pages/index';
 
 function Main() {
   return (
