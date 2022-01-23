49cbc5293cbf567ef3708fbf3fb061cd8fe94335
jeessh <jesshuang5733@gmail.com>
2022-01-23 14:24:27 -0500

    finish elements

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -15,7 +15,7 @@ export const ServicesContainer = styled.div`
     @media screen and (max-width: 480px) {
         height: 1300px;
     }
-`
+`;
 
 export const ServicesWrapper = styled.div`
     max-width: 1000px;
@@ -34,7 +34,7 @@ export const ServicesWrapper = styled.div`
         grid-template-columns: 1fr;
         padding: 0 20px;
     }
-`
+`;
 
 export const ServicesCard= styled.div`
     background: #fff;
@@ -52,13 +52,13 @@ export const ServicesCard= styled.div`
         transition: all 0.2s ease-in-out;
         cursor: pointer;
     }
-`
+`;
 
 export const ServicesIcon = styled.img`
     height: 160px;
     width: 160px;
     margin-bottom: 10px;
-`
+`;
 
 export const ServicesH1 = styled.h1`
     font-size: 2.5rem;
@@ -68,4 +68,14 @@ export const ServicesH1 = styled.h1`
     @media screen and (max-width: 480px) {
         font-size: 2rem;
     }
-`
+`;
+
+export const ServicesH2 = styled.h2`
+    font-size: 1rem;
+    margin-bottom: 10px;
+`;
+
+export const ServicesP = styled.p`
+    font-size: 1rem;
+    text-align: center
+`;
