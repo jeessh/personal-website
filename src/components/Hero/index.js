5571095422c2c3d4d86100b17e53b43ef436ae84
jeessh <jesshuang5733@gmail.com>
2022-01-24 00:02:11 -0500

    adding info

---
+++ src/components/Hero/index.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+import Video from '../../videos/video.mp4'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
+import { Button } from '../ButtonElements'
+
+const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Hey there!</HeroH1>
+        <HeroP>Welcome to my personal site! *Of course it's still
+          in development, but feel free to check browse through my progress :D </HeroP>
+        <HeroBtnWrapper>
+          <Button to='resume' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'               smooth={true} duration={500} spy={true} exact='true' offset={-80}>
+            Resume {hover ? <ArrowForward /> : <ArrowRight/>}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  )
+}
+
+export default Hero
